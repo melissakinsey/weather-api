b750c74a964baf651ec578aab92d211305bc3670
Melissa.Kinsey <kinsey.melissa@gmail.com>
2020-12-13 18:33:13 -0600

    feat: Add ability to look up weather by city.

--- script.js
+++ script.js
@@ -1,110 +1,36 @@
 $(document).ready(function () {
-  
-const log = document.getElementById('values');
+  // {/* <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
+  // <script type="text/javascript"> */}
 
-input.addEventListener('input', updateValue);
+  $("#values").click(function () {
+    var APIKey = "addc5f504176245d7ca85381d3c3c7bf";
+    var city = $("#cityInput").val();
+    console.log(city);
+    var queryURL =
+      "https://api.openweathermap.org/data/2.5/forecast?q=" +
+      city +
+      "&appid=" +
+      APIKey;
+    $.ajax({
+      url: queryURL,
+      method: "GET",
+    }).then(function (response) {
+      console.log(queryURL);
 
-function updateValue(e) {
-  log.textContent = e.target.value;
-}
+      console.log(response);
 
-let queryURL = "https://api.openweathermap.org/data/2.5/forecast?q={city name},{state code},{country code}&appid=9c31a3b4618fe0ad90a6912f6418fb0c"
+      $(".city").html("<h1>" + response.name + " Weather Details</h1>");
+      $(".wind").text("Wind Speed: " + response.wind.speed);
+      $(".humidity").text("Humidity: " + response.main.humidity);
 
-        var city = $(this).attr("data-name");
-        var queryURL = "https://api.openweathermap.org/data/2.5/forecast?q=" + city + "&apikey=9c31a3b4618fe0ad90a6912f6418fb0c";
+      var tempF = (response.main.temp - 273.15) * 1.8 + 32;
 
-        // Creating an AJAX call for the specific city button being clicked
-        $.ajax({
-          url: queryURL,
-          method: "GET"
-        }).then(function(response) {
+      $(".temp").text("Temperature (K) " + response.main.temp);
+      $(".tempF").text("Temperature (F) " + tempF.toFixed(2));
 
-          // Creating a div to hold the forecast
-          var movieDiv = $("<div class='movie'>");
-
-          // Storing the rating data
-          var rating = response.Rated;
-
-          // Creating an element to have the rating displayed
-          var pOne = $("<p>").text("Rating: " + rating);
-
-          // Displaying the rating
-          movieDiv.append(pOne);
-
-          // Storing the release year
-          var released = response.Released;
-
-          // Creating an element to hold the release year
-          var pTwo = $("<p>").text("Released: " + released);
-
-          // Displaying the release year
-          movieDiv.append(pTwo);
-
-          // Storing the plot
-          var plot = response.Plot;
-
-          // Creating an element to hold the plot
-          var pThree = $("<p>").text("Plot: " + plot);
-
-          // Appending the plot
-          movieDiv.append(pThree);
-
-          // Retrieving the URL for the image
-          var imgURL = response.Poster;
-
-          // Creating an element to hold the image
-          var image = $("<img>").attr("src", imgURL);
-
-          // Appending the image
-          movieDiv.append(image);
-
-          // Putting the entire movie above the previous movies
-          $("#movies-view").prepend(movieDiv);
-        });
-
-      }
-
-      // Function for displaying movie data
-      function renderButtons() {
-
-        // Deleting the movies prior to adding new movies
-        // (this is necessary otherwise you will have repeat buttons)
-        $("#buttons-view").empty();
-
-        // Looping through the array of movies
-        for (var i = 0; i < movies.length; i++) {
-
-          // Then dynamicaly generating buttons for each movie in the array
-          // This code $("<button>") is all jQuery needs to create the beginning and end tag. (<button></button>)
-          var a = $("<button>");
-          // Adding a class of movie-btn to our button
-          a.addClass("movie-btn");
-          // Adding a data-attribute
-          a.attr("data-name", movies[i]);
-          // Providing the initial button text
-          a.text(movies[i]);
-          // Adding the button to the buttons-view div
-          $("#buttons-view").append(a);
-        }
-      }
-
-      // This function handles events where a movie button is clicked
-      $("#add-movie").on("click", function(event) {
-        event.preventDefault();
-        // This line grabs the input from the textbox
-        var movie = $("#movie-input").val().trim();
-
-        // Adding movie from the textbox to our array
-        movies.push(movie);
-
-        // Calling renderButtons which handles the processing of our movie array
-        renderButtons();
-      });
-
-      // Adding a click event listener to all elements with a class of "movie-btn"
-      $(document).on("click", ".movie-btn", displayMovieInfo);
-
-      // Calling the renderButtons function to display the initial buttons
-      renderButtons();
-    
-  
+      console.log("Wind Speed: " + response.wind.speed);
+      console.log("Humidity: " + response.main.humidity);
+      console.log("Temperature (F): " + tempF);
+    });
+  });
+});
