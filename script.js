d5e10efe9a350cd62bcc8277416267b6bfa6dd42
Melissa.Kinsey <kinsey.melissa@gmail.com>
2020-12-14 19:28:38 -0600

    feat: Call weather description forDay 1 of five-day forecast.

--- script.js
+++ script.js
@@ -15,51 +15,45 @@ $(document).ready(function () {
     }).then(function (list) {
       console.log(queryURL);
 
-      console.log(list);
+      console.log(list.list[0].weather[0].description);
+      var description = list.list[0].weather[0].description;
+      $("#description-day-1").text(description);
       $(".city").html("<h2><b>" + list.city.name + " Weather Details</b></h2>");
-      
-      function uvIndex (lat, lon) {
-        let queryURL =
-          "https://api.openweathermap.org/data/2.5/uvi?lat=" +
-          lat +
-          "&lon=" +
-          lon +
-          "&appid=" +
-          APIKey;
-        
-//       //   $("#values").click(function () {
-//       //     var APIKey = "addc5f504176245d7ca85381d3c3c7bf";
-//       //     var cityName = $("#cityInput").val();
-//       //     console.log(cityName);
-//       //     var queryURL =
-//       //       "https://api.openweathermap.org/data/2.5/forecast/daily?q=" +
-//       //       cityName +
-//       //       "&cnt=5" +
-//       //       "&appid=" +
-//       //       APIKey;
-//       //
-//       //     $.ajax({
-//       //       url: queryURL,
-//       //       method: "GET",
-//       //     }).then(function (list) {
-//       //       console.log(queryURL);
-//       //
-//       //       console.log(list);
-//       //       $(".city").html("<h2><b>" + list.city.name + " Weather Details</b></h2>");
-// 
-//       // console.log(list);
-//       // var Windspeed = response.list.wind.speed;
-//       // // const theWind = ".wind";
-//       // $(".wind").html("<p>Windspeed: " + Windspeed + "</p>");
-//       //
-//       //       console.log();
-//       //       $(".humidity").text("Humidity: " + list.main.humidity);
-//       //
-//       //       console.log(list);
-//       //       $(".tempF").text("Temperature (F) " + list.main.temperature);
-// 
-//       // console.log("Humidity: " + response.list.main.humidity);
-//       // console.log("Temperature (F): " + tempF);
-//     });
-//   });
-// });
+      //
+
+      //   $("#values").click(function () {
+      //     var APIKey = "addc5f504176245d7ca85381d3c3c7bf";
+      //     var cityName = $("#cityInput").val();
+      //     console.log(cityName);
+      //     var queryURL =
+      //       "https://api.openweathermap.org/data/2.5/forecast/daily?q=" +
+      //       cityName +
+      //       "&cnt=5" +
+      //       "&appid=" +
+      //       APIKey;
+      //
+      //     $.ajax({
+      //       url: queryURL,
+      //       method: "GET",
+      //     }).then(function (list) {
+      //       console.log(queryURL);
+      //
+      //       console.log(list);
+      //       $(".city").html("<h2><b>" + list.city.name + " Weather Details</b></h2>");
+
+      // console.log(response);
+      // var Windspeed = response.list.wind.speed;
+      // // const theWind = ".wind";
+      // $(".wind").html("<p>Windspeed: " + Windspeed + "</p>");
+      //
+      //       console.log();
+      //       $(".humidity").text("Humidity: " + list.main.humidity);
+      //
+      //       console.log(list);
+      //       $(".tempF").text("Temperature (F) " + list.main.temperature);
+
+      // console.log("Humidity: " + response.list.main.humidity);
+      // console.log("Temperature (F): " + tempF);
+    });
+  });
+});
