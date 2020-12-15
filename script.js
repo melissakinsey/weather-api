dad72f38d08c8686783a637a70be6f5eb04927f0
Melissa.Kinsey <kinsey.melissa@gmail.com>
2020-12-15 17:09:57 -0600

    feat: API returns temperature in degrees Celsius.

--- script.js
+++ script.js
@@ -15,11 +15,33 @@ $(document).ready(function () {
     }).then(function (list) {
       console.log(queryURL);
 
-      console.log(list.list[0].weather[0].description);
-      var description = list.list[0].weather[0].description;
-      $("#description-day-1").text(description);
+      console.log(list);
       $(".city").html("<h2><b>" + list.city.name + " Weather Details</b></h2>");
-      //
+      console.log(list.list[0].weather[0].description);
+      var descriptionOne = list.list[0].weather[0].description;
+      $("#description-day-1").text(descriptionOne);
+
+      console.log(list.list[0].main.temp);
+      var tempOne = list.list[0].main.temp;
+      $("#temp-day-1").text(tempOne);
+
+      console.log(list.list[8].weather[0].description);
+      var descriptionTwo = list.list[8].weather[0].description;
+      $("#description-day-2").text(descriptionTwo);
+
+      console.log(list.list[16].weather[0].description);
+      var descriptionThree = list.list[16].weather[0].description;
+      $("#description-day-3").text(descriptionThree);
+
+      console.log(list.list[24].weather[0].description);
+      var descriptionFour = list.list[24].weather[0].description;
+      $("#description-day-4").text(descriptionFour);
+
+      console.log(list.list[32].weather[0].description);
+      var descriptionFive = list.list[32].weather[0].description;
+      $("#description-day-5").text(descriptionFive);
+
+      // $(".city").html("<h2><b>" + list.city.name + " Weather Details</b></h2>");
 
       //   $("#values").click(function () {
       //     var APIKey = "addc5f504176245d7ca85381d3c3c7bf";
