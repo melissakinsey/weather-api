6c3dd68cb78a4bed3d26c69a45d910a1ecad6eb7
Melissa.Kinsey <kinsey.melissa@gmail.com>
2020-12-17 15:35:32 -0600

    feat: Add functionality so that Day 1 forecast appears in sidenav when a city is searched. style: Finish styling transparent background, position, and type styles of cards.

--- script.js
+++ script.js
@@ -4,12 +4,7 @@ $(document).ready(function () {
     var city = $("#cityInput").val();
     console.log(city);
     var cnt = 5;
-    // var queryURL =
-    //   "https://api.openweathermap.org/data/2.5/forecast?q=" +
-    //   city +
-    //   "&units=imperial" +
-    //   "&appid=" +
-    //   APIKey;
+
     var queryURL =
       "http://api.openweathermap.org/data/2.5/forecast?q=" +
       city +
@@ -18,12 +13,6 @@ $(document).ready(function () {
       "&units=imperial" +
       "&appid=" +
       API_key;
-    // var queryURL =
-    //   "https://api.openweathermap.org/data/2.5/forecast/daily?q=" +
-    //   city +
-    //   cnt +
-    //   "&appid=" +
-    //   API_key;
 
     $.ajax({
       url: queryURL,
@@ -31,118 +20,112 @@ $(document).ready(function () {
     }).then(function (list) {
       console.log(queryURL);
 
-      console.log(list);
-      $(".city").html("<h2><b>" + list.city.name + " Weather Details</b></h2>");
+      $.ajax({
+        url:
+          "https://api.openweathermap.org/data/2.5/uvi?lat=37.75&lon=-122.37&apikey=addc5f504176245d7ca85381d3c3c7bf",
+        method: "GET",
+      }).then(function (uvi) {
+        console.log(uvi);
+        $(".city").html(
+          "<h2><b>" + list.city.name + " Weather Details</b></h2>"
+        );
+        //BEGIN DAY ONE
 
-      console.log(list.list[0].weather[0].description);
-      var descriptionOne = list.list[0].weather[0].description;
-      $("#description-day-1").text(descriptionOne);
+        console.log(list.list[0].weather[0].description);
+        var descriptionOne = list.list[0].weather[0].description;
+        $("#description-day-1").text(descriptionOne);
 
-      console.log(list.list[0].main.temp);
-      var tempOne = list.list[0].main.temp;
-      $("#temp-day-1").text(tempOne);
+        console.log(list.list[0].main.temp);
+        var tempOne = list.list[0].main.temp;
+        $("#temp-day-1").text(tempOne);
+        $("#temperature").text("Temp (°F): " + tempOne);
 
-      console.log(list.list[0].main.humidity);
-      var humidityOne = list.list[0].main.humidity;
-      $("#humidity-day-1").text(humidityOne);
+        console.log(list.list[0].main.humidity);
+        var humidityOne = list.list[0].main.humidity;
+        $("#humidity-day-1").text(humidityOne);
+        $("#hot-tub").text("Humidity: " + humidityOne);
 
-      console.log(list.list[0].wind.speed);
-      var windspeedOne = list.list[0].wind.speed;
-      $("#windspeed-day-1").text(windspeedOne);
+        console.log(list.list[0].wind.speed);
+        var windspeedOne = list.list[0].wind.speed;
+        $("#windspeed-day-1").text(windspeedOne);
+        $("#kite").text("Windspeed: " + windspeedOne);
 
-      //BEGIN DAY 2
-      console.log(list.list[1].weather[0].description);
-      var descriptionTwo = list.list[1].weather[0].description;
-      $("#description-day-2").text(descriptionTwo);
+        console.log(uvi.value);
+        var uvIndex = uvi.value;
+        $("#uvi-day-1").text(uvIndex);
+        $("#sunglasses").text("UV Index: " + uvIndex);
 
-      console.log(list.list[1].main.temp);
-      var tempTwo = list.list[1].main.temp;
-      $("#temp-day-2").text(tempTwo);
+        //BEGIN DAY 2
+        console.log(list.list[1].weather[0].description);
+        var descriptionTwo = list.list[1].weather[0].description;
+        $("#description-day-2").text(descriptionTwo);
 
-      console.log(list.list[1].main.humidity);
-      var humidityTwo = list.list[1].main.humidity;
-      $("#humidity-day-2").text(humidityTwo);
+        console.log(list.list[1].main.temp);
+        var tempTwo = list.list[1].main.temp;
+        $("#temp-day-2").text(tempTwo);
 
-      console.log(list.list[1].wind.speed);
-      var windspeedTwo = list.list[1].wind.speed;
-      $("#windspeed-day-2").text(windspeedTwo);
+        console.log(list.list[1].main.humidity);
+        var humidityTwo = list.list[1].main.humidity;
+        $("#humidity-day-2").text(humidityTwo);
 
-      //BEGIN DAY 3
+        console.log(list.list[1].wind.speed);
+        var windspeedTwo = list.list[1].wind.speed;
+        $("#windspeed-day-2").text(windspeedTwo);
 
-      console.log(list.list[2].weather[0].description);
-      var descriptionThree = list.list[2].weather[0].description;
-      $("#description-day-3").text(descriptionThree);
+        //BEGIN DAY 3
 
-      console.log(list.list[2].main.temp);
-      var tempThree = list.list[2].main.temp;
-      $("#temp-day-3").text(tempThree);
+        console.log(list.list[2].weather[0].description);
+        var descriptionThree = list.list[2].weather[0].description;
+        $("#description-day-3").text(descriptionThree);
 
-      console.log(list.list[2].main.humidity);
-      var humidityThree = list.list[2].main.humidity;
-      $("#humidity-day-3").text(humidityThree);
+        console.log(list.list[2].main.temp);
+        var tempThree = list.list[2].main.temp;
+        $("#temp-day-3").text(tempThree);
 
-      console.log(list.list[2].wind.speed);
-      var windspeedThree = list.list[2].wind.speed;
-      $("#windspeed-day-3").text(windspeedThree);
+        console.log(list.list[2].main.humidity);
+        var humidityThree = list.list[2].main.humidity;
+        $("#humidity-day-3").text(humidityThree);
 
-      //BEGIN DAY 4
+        console.log(list.list[2].wind.speed);
+        var windspeedThree = list.list[2].wind.speed;
+        $("#windspeed-day-3").text(windspeedThree);
 
-      console.log(list.list[3].weather[0].description);
-      var descriptionFour = list.list[3].weather[0].description;
-      $("#description-day-4").text(descriptionFour);
+        //BEGIN DAY 4
 
-      console.log(list.list[3].main.temp);
-      var tempFour = list.list[3].main.temp;
-      $("#temp-day-4").text(tempFour);
+        console.log(list.list[3].weather[0].description);
+        var descriptionFour = list.list[3].weather[0].description;
+        $("#description-day-4").text(descriptionFour);
 
-      console.log(list.list[3].main.humidity);
-      var humidityFour = list.list[3].main.humidity;
-      $("#humidity-day-4").text(humidityFour);
+        console.log(list.list[3].main.temp);
+        var tempFour = list.list[3].main.temp;
+        $("#temp-day-4").text(tempFour);
 
-      console.log(list.list[3].wind.speed);
-      var windspeedFour = list.list[3].wind.speed;
-      $("#windspeed-day-4").text(windspeedFour);
+        console.log(list.list[3].main.humidity);
+        var humidityFour = list.list[3].main.humidity;
+        $("#humidity-day-4").text(humidityFour);
 
-      //BEGIN DAY 5
+        console.log(list.list[3].wind.speed);
+        var windspeedFour = list.list[3].wind.speed;
+        $("#windspeed-day-4").text(windspeedFour);
 
-      console.log(list.list[4].weather[0].description);
-      var descriptionFive = list.list[4].weather[0].description;
-      $("#description-day-5").text(descriptionFive);
+        //BEGIN DAY 5
 
-      console.log(list.list[4].main.temp);
-      var tempFive = list.list[4].main.temp;
-      $("#temp-day-5").text(tempFive);
+        console.log(list.list[4].weather[0].description);
+        var descriptionFive = list.list[4].weather[0].description;
+        $("#description-day-5").text(descriptionFive);
 
-      console.log(list.list[4].main.humidity);
-      var humidityFive = list.list[4].main.humidity;
-      $("#humidity-day-5").text(humidityFive);
+        console.log(list.list[4].main.temp);
+        var tempFive = list.list[4].main.temp;
+        $("#temp-day-5").text(tempFive);
 
-      console.log(list.list[4].wind.speed);
-      var windspeedFive = list.list[4].wind.speed;
-      $("#windspeed-day-5").text(windspeedFive);
+        console.log(list.list[4].main.humidity);
+        var humidityFive = list.list[4].main.humidity;
+        $("#humidity-day-5").text(humidityFive);
 
-      //       console.log(list);
-      //       $(".tempF").text("Temperature (F) " + list.main.temperature);
-
-      //
-      //
-      //
-      //
-      //       console.log(list.list[8].weather[0].description);
-      //       var descriptionTwo = list.list[8].weather[0].description;
-      //       $("#description-day-2").text(descriptionTwo);
-      //
-      //       console.log(list.list[16].weather[0].description);
-      //       var descriptionThree = list.list[16].weather[0].description;
-      //       $("#description-day-3").text(descriptionThree);
-      //
-      //       console.log(list.list[24].weather[0].description);
-      //       var descriptionFour = list.list[24].weather[0].description;
-      //       $("#description-day-4").text(descriptionFour);
-      //
-      //       console.log(list.list[32].weather[0].description);
-      //       var descriptionFive = list.list[32].weather[0].description;
-      //       $("#description-day-5").text(descriptionFive);
+        console.log(list.list[4].wind.speed);
+        var windspeedFive = list.list[4].wind.speed;
+        $("#windspeed-day-5").text(windspeedFive);
+      });
     });
   });
 });
